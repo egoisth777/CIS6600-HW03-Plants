b6ab8f25ae628c61751d981aab9726b05dee9a6c
Yueyang Li <egoistheresy@outlook.com>
2025-02-08 15:26:35 -0500

    Initial Commit

---
+++ src/randomNode.py
@@ -0,0 +1,87 @@
+# randomNode.py
+#   Produces random locations to be used with the Maya instancer node.
+
+import sys
+import random
+
+import maya.OpenMaya as OpenMaya
+import maya.OpenMayaAnim as OpenMayaAnim
+import maya.OpenMayaMPx as OpenMayaMPx
+import maya.cmds as cmds
+
+# Useful functions for declaring attributes as inputs or outputs.
+def MAKE_INPUT(attr):
+    attr.setKeyable(True)
+    attr.setStorable(True)
+    attr.setReadable(True)
+    attr.setWritable(True)
+def MAKE_OUTPUT(attr):
+    attr.setKeyable(False)
+    attr.setStorable(False)
+    attr.setReadable(True)
+    attr.setWritable(False)
+
+# Define the name of the node
+kPluginNodeTypeName = "randomNode"
+
+# Give the node a unique ID. Make sure this ID is different from all of your
+# other nodes!
+randomNodeId = OpenMaya.MTypeId(0x8704)
+
+# Node definition
+class randomNode(OpenMayaMPx.MPxNode):
+    # Declare class variables:
+    # TODO:: declare the input and output class variables
+    #         i.e. inNumPoints = OpenMaya.MObject()
+    
+    # constructor
+    def __init__(self):
+        OpenMayaMPx.MPxNode.__init__(self)
+
+    # compute
+    def compute(self,plug,data):
+        # TODO:: create the main functionality of the node. Your node should 
+        #         take in three floats for max position (X,Y,Z), three floats 
+        #         for min position (X,Y,Z), and the number of random points to
+        #         be generated. Your node should output an MFnArrayAttrsData 
+        #         object containing the random points. Consult the homework
+        #         sheet for how to deal with creating the MFnArrayAttrsData. 
+
+        data.setClean(plug)
+    
+# initializer
+def nodeInitializer():
+    tAttr = OpenMaya.MFnTypedAttribute()
+    nAttr = OpenMaya.MFnNumericAttribute()
+
+    # TODO:: initialize the input and output attributes. Be sure to use the 
+    #         MAKE_INPUT and MAKE_OUTPUT functions.
+
+
+    try:
+        # TODO:: add the attributes to the node and set up the
+        #         attributeAffects (addAttribute, and attributeAffects)
+        print "Initialization!\n"
+
+    except:
+        sys.stderr.write( ("Failed to create attributes of %s node\n", kPluginNodeTypeName) )
+
+# creator
+def nodeCreator():
+    return OpenMayaMPx.asMPxPtr( randomNode() )
+
+# initialize the script plug-in
+def initializePlugin(mobject):
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    try:
+        mplugin.registerNode( kPluginNodeTypeName, randomNodeId, nodeCreator, nodeInitializer )
+    except:
+        sys.stderr.write( "Failed to register node: %s\n" % kPluginNodeTypeName )
+
+# uninitialize the script plug-in
+def uninitializePlugin(mobject):
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    try:
+        mplugin.deregisterNode( randomNodeId )
+    except:
+        sys.stderr.write( "Failed to unregister node: %s\n" % kPluginNodeTypeName )
